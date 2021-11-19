5933a6e34aa5aa70f2344b325cb8807233362a27
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-11-19 19:25:43 -0300

    adicionado a estrutura do projeto de produtos.

---
+++ Jr.Backend.Produtos.WorkerService/DependencyInjection/ServicesDependency.cs
@@ -0,0 +1,38 @@
+﻿using GreenPipes;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Jr.Backend.Produtos.WorkerService.DependencyInjection
+{
+    public static class ServicesDependency
+    {
+        public static void AddServiceDependencyWorkerService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                 {
+                     cfg.Host(new Uri(configuration["RabbitSetting:UriBase"]), h =>
+                     {
+                         h.Username(configuration["RabbitSetting:User"]);
+                         h.Password(configuration["RabbitSetting:Password"]);
+                     });
+
+                     cfg.ReceiveEndpoint("FornecedorCadastradoEvent", ep =>
+                     {
+                         ep.PrefetchCount = 10;
+                         ep.UseMessageRetry(r => r.Interval(2, 100));
+                     });
+                     cfg.ReceiveEndpoint("FornecedorAtualizadoEvent", ep =>
+                     {
+                         ep.PrefetchCount = 10;
+                         ep.UseMessageRetry(r => r.Interval(2, 100));
+                     });
+                 }));
+            });
+            services.AddMassTransitHostedService();
+        }
+    }
+}
---
+++ Jr.Backend.Produtos.WorkerService/Program.cs
@@ -0,0 +1,34 @@
+using Jr.Backend.Produtos.WorkerService.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+
+namespace Jr.Backend.Produtos.WorkerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")?.ToLowerInvariant();
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddServiceDependencyWorkerService(config);
+                });
+        }
+    }
+}
