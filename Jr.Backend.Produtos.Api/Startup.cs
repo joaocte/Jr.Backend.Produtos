c3023cf788545b7343f8ac0bc8b362e7018a5e60
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-11-27 07:40:04 -0300

    Adicionado o basico para criação do template.

--- Jr.Backend.Produtos.Api/Startup.cs
+++ Jr.Backend.Produtos.Api/Startup.cs
@@ -1,24 +1,35 @@
+using Jr.Backend.Libs.API.Abstractions;
+using Jr.Backend.Libs.API.DependencyInjection;
+using Jr.Backend.Libs.Framework.DependencyInjection;
+using Jr.Backend.Libs.Security.Abstractions.Infrastructure.Interfaces;
+using Jr.Backend.Libs.Security.DependencyInjection;
+using Jr.Backend.Libs.Security.Infrastructure;
+using Jr.Backend.Libs.Security.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Jr.Backend.Produtos.Api
 {
     public class Startup
     {
+        private readonly IJrApiOption jrApiOption;
+        private readonly ISecurityConfiguration serSecurityConfiguration;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            jrApiOption = new JrApiOption
+            {
+                Title = "Jr.Backend.Produtos.Api",
+                Description = "Api de Cadastro de Produtos",
+                Email = "joaocte@gmail.com",
+                Uri = "https://github.com/joaocte/Jr.Backend.Produto",
+            };
+            serSecurityConfiguration =
+                new SecurityConfiguration("mongodb://localhost:27017/?authSource=admin", "JrTenant");
         }
 
         public IConfiguration Configuration { get; }
@@ -26,12 +37,10 @@ namespace Jr.Backend.Produtos.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Jr.Backend.Produtos.Api", Version = "v1" });
-            });
+            services.AddServiceDependencyJrApiSwagger(Configuration, () => jrApiOption);
+            services.AddServiceDependencyJrFramework();
+            services.AddServiceDependencyJrSecurityApiUsingCustomValidate(() => serSecurityConfiguration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,15 +49,10 @@ namespace Jr.Backend.Produtos.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jr.Backend.Produtos.Api v1"));
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
+            app.UseJrApiSwaggerSecurity(env, () => jrApiOption);
+            app.UseSecurity();
 
             app.UseEndpoints(endpoints =>
             {
