587a7156e3cb2588ca8d497349e986729d82cddb
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-11-30 18:13:15 -0300

    Entidade de Produto

---
+++ Jr.Backend.Produtos.Domain/Produto.cs
@@ -0,0 +1,16 @@
+﻿using Jr.Backend.Libs.Domain.Abstractions;
+using System;
+
+namespace Jr.Backend.Produtos.Domain
+{
+    public class Produto : Entity
+    {
+        public string NomeProduto { get; private set; }
+        public long Quantidade { get; private set; }
+        public long QuantidadeMinimaEstoque { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public Guid FornecedorId { get; private set; }
+        public string Descricao { get; private set; }
+        public long LimiteMaximoPorPedido { get; private set; }
+    }
+}
